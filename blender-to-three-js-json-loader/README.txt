40c9ed592c49d63015caad22f57aca71ea9c17fd
Ankur Ankit <ankur.ankit3@gmail.com>
2015-12-16 23:33:51 +0530

    cad_json

---
+++ blender-to-three-js-json-loader/README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/wpdildine/pen/MwyYxo.
+
+ Scene was originally created in Blender.  Custom mesh faces applied.
---
+++ try.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script type="text/javascript" src="js/three.js"></script>
+<script type="text/javascript" src="js/dat.gui.min.js"></script>
+</head>
+<body>
+	<div id="webGL-container">
+		
+	</div>
+	<script type="text/javascript" src="js/main.js"></script>
+</body>
+</html>
